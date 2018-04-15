de14db2b72b33e52bb9244e1d30611e1a508e9e5
BazzaCipher <33623276+BazzaCipher@users.noreply.github.com>
2018-04-15 19:56:07 +1000

    Funny little easter egg
    Fixed footer and added Doughnut easter egg in function eatDonut <3

--- Insertefficientname.js
+++ Insertefficientname.js
@@ -1,3 +1,14 @@
+	var donutBite = 0
 function redirectTo(sUrl) {
-	window.location.href = (sUrl)
+	window.location.href = (sUrl);
 	}
+function eatDonut() {
+	donutBite += 1;
+	if (donutBite != 4) {
+		document.getElementById('donut').src = "Doughnut-eaten" + donutBite + ".png"
+	} else {
+		donutBite = 0
+		alert("Here's another one, made just for you <3")
+		document.getElementById('donut').src = "Doughnut.png"
+	}
+}
